b8fafa071b1e476b62b1b7f529183038fc495ee4
SaDGaBrY <72394984+SaDGaBrY@users.noreply.github.com>
2021-12-27 22:33:35 +0100

    up

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client(
     {intents:["GUILDS","GUILD_MEMBERS","GUILD_MESSAGES"]}
 )
 
-client.login("OTI1MDIyMDg3OTA5ODcxNjY2.YcnD8w.DnO2Uahm9n0SEkxjSIU6EBhEZos")
+client.login(process.env.token)
 
 client.on("messageCreate", (message)=>{
         if(message.content == "!youtube"){
